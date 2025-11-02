7e31f80aa052ae5f2c53f2bbf4f0edb81b992444
Antarash10 <146330332+Antarash10@users.noreply.github.com>
2025-11-02 15:37:15 +0600

    Create LT1.php

---
+++ Mid/LabTask1/LT1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<head>
+    <title>Registration Form</title>
+</head>
+
+<body>
+    <center>
+
+<h2 style = "color : blue">Clinic Patient Registration</h2>
+</center>
+
+Full Name:<br></br>
+<input type ="text"><br></br>
+Age :<br></br>
+<input type ="number"><br></br>
+Phone number :<br></br>
+<input type ="number"><br></br>
+Email Address :<br></br>
+<input type ="text"><br></br>
+Insurance Provider:<br></br>
+<br>
+<select>
+    <option>select provider </option>
+</select>
+</br>
+<input type ="text"><br></br>
+
+Insurance Policy Number:<br></br>
+<input type ="text"><br></br>
+<center>
+<h2 style = "color : blue">Clinic Patient Registration</h2>
+</center>
+
+User name:<br></br>
+<input type ="text"><br></br>
+Password:<br></br>
+<input type ="text"><br></br>
+Confirm Password:<br></br>
+<input type ="text"><br></br>
+
+<center> 
+    <input type ="Submit">
+</center>
+
+</body>
